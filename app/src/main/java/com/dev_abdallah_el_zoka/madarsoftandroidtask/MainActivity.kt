96a39dc1b4d18ba7cc95c0973a066814b56b0612
Abdallah El-Zoka <abdallah.elzoka@gmail.com>
2021-06-24 17:05:39 +0200

    Initial Commit

---
+++ app/src/main/java/com/dev_abdallah_el_zoka/madarsoftandroidtask/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dev_abdallah_el_zoka.madarsoftandroidtask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
